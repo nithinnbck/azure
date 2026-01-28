742fbfa32df83316bf68f64f51668449c05c8322
nithinnbck <nithinnbck@gmail.com>
2026-01-28 22:46:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
 // Export app for testing
 module.exports = app;
 
-// Only start server if not in test environment
+// Only start server if not in test  environment
 if (process.env.NODE_ENV !== 'test') {
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
