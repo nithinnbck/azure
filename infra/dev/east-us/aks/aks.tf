d350911598e3298d359342dfdb163d14366ce47a
nithinnbck <nithinnbck@gmail.com>
2026-01-28 21:17:11 +0530

    Update aks.tf

--- infra/dev/east-us/aks/aks.tf
+++ infra/dev/east-us/aks/aks.tf
@@ -10,7 +10,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     vm_size             = "Standard_DS2_v2"
     min_count           = 1
     max_count           = 3
-    enable_auto_scaling = true
+    #enable_auto_scaling = true
     type                = "VirtualMachineScaleSets"
   }
 
@@ -23,22 +23,6 @@ resource "azurerm_kubernetes_cluster" "aks" {
     load_balancer_sku = "standard"
   }
 
-  role_based_access_control {
-    enabled = true
-    azure_active_directory {
-      managed                = true
-      admin_group_object_ids = var.aad_admin_group_object_ids
-    }
-  }
-
-  addon_profile {
-    kube_dashboard {
-      enabled = false
-    }
-    azure_policy {
-      enabled = true
-    }
-  }
 
   tags = {
     environment = var.environment
