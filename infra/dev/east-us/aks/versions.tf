d8d65abf81020a9d542ae24decee0ba138aded0c
nithinnbck <nithinnbck@gmail.com>
2026-01-28 20:04:07 +0530

    feat: version.tf

---
+++ infra/dev/east-us/aks/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.80.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstatestorage"
+    container_name       = "tfstate"
+    key                  = "aks-cluster.terraform.tfstate"
+  }
+}
