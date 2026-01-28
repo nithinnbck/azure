d6614ee0b5a6b5fc5b255c6557b54b7ac5f95611
nithinnbck <nithinnbck@gmail.com>
2026-01-28 21:18:26 +0530

    Update output.tf

--- infra/dev/east-us/aks/output.tf
+++ infra/dev/east-us/aks/output.tf
@@ -2,10 +2,7 @@ output "aks_cluster_name" {
   value = azurerm_kubernetes_cluster.aks.name
 }
 
-output "aks_kube_config" {
-  value = azurerm_kubernetes_cluster.aks.kube_config[0].raw_kube_config
-  sensitive = true
-}
+
 
 output "aks_fqdn" {
   value = azurerm_kubernetes_cluster.aks.fqdn
