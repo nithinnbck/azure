11159e524a1aacf628cef924c73312516bb7d803
nithinnbck <nithinnbck@gmail.com>
2026-01-28 21:43:37 +0530

    Update aks.tf

--- infra/dev/east-us/aks/aks.tf
+++ infra/dev/east-us/aks/aks.tf
@@ -8,9 +8,6 @@ resource "azurerm_kubernetes_cluster" "aks" {
     name                = "default"
     node_count          = 2
     vm_size             = "Standard_DS2_v2"
-    min_count           = 1
-    max_count           = 3
-    enable_auto_scaling = true
     type                = "VirtualMachineScaleSets"
   }
 
